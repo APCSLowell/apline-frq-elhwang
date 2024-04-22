d95e0af5e9525f87cfdb560d47d12791870da888
elhwang <143028051+elhwang@users.noreply.github.com>
2024-04-22 13:25:16 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,19 @@
 public class APLine
 {
-  /* your code here */
-  
+  private Aval;
+  private Bval;
+  private Cval;
+ public APLine(int a, int b, int c){
+    Aval = a;
+    Bval = b;
+    Cval = c;
+  }
+  public double getSlope(){
+    return (double)a/b
+  }
+  public boolean OnLine(int x, int y){
+    if(((a*x) + (b*y) + c) == 0)
+      return true;
+    return false;
+  }
 }
